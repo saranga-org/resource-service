209560a19ed7926bda70660fc2959a2a822616b3
DewramJay <jayasinghe_jaadk_e21@engug.ruh.ac.lk>
2024-09-17 12:41:44 +0530

    vehicle

--- src/main/java/FuelPass/Dev/FuelPass/Repo/VehicleRepo.java
+++ src/main/java/FuelPass/Dev/FuelPass/Repo/VehicleRepo.java
@@ -3,5 +3,6 @@ package FuelPass.Dev.FuelPass.Repo;
 import FuelPass.Dev.FuelPass.Entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface VehicleRepo extends JpaRepository<Vehicle, Integer> {
 }
