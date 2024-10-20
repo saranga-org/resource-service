480d910aa605d386341488b5f58003e65fbb90c7
Saranga-B <bandarasaranga@icloud.com>
2024-10-20 20:50:43 +0530

    service-access-controller-changed

--- src/main/java/FuelPass/Dev/FuelPass/Controller/ServiceAccessController.java
+++ src/main/java/FuelPass/Dev/FuelPass/Controller/ServiceAccessController.java
@@ -11,13 +11,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/serviceAccess")
+@RequestMapping("/api")
 public class ServiceAccessController {
     @Autowired
     public JwtService jwtService;
@@ -26,20 +23,17 @@ public class ServiceAccessController {
     @Autowired
     private  AuthenticationService service;
 
-
-
-        @PostMapping("/fuelQuota")
-        public ResponseEntity<AuthorizationResponse> fuelQuota() {
-            try{
-                return ResponseEntity.ok(serviceAccessService.getOperator(service.getCurrentUser()));
-            }catch(RuntimeException e){
-                return ResponseEntity.ok(serviceAccessService.getOperator(service.getCurrentUser()));
-            }
-
+    @GetMapping("/fuel-quota")
+    public ResponseEntity<AuthorizationResponse> fuelQuota() {
+        try{
+            return ResponseEntity.ok(serviceAccessService.getOperator(service.getCurrentUser()));
+        }catch(RuntimeException e){
+            return ResponseEntity.ok(serviceAccessService.getOperator(service.getCurrentUser()));
         }
 
+    }
 
-    @PostMapping("/vehicleService")
+    @GetMapping("/vehicle-service")
     public ResponseEntity<AuthorizationResponse> vehicleService(){
         try{
             return ResponseEntity.ok(serviceAccessService.getUser(service.getCurrentUser()));
--- src/main/java/FuelPass/Dev/FuelPass/Controller/UserVerificationController.java
+++ src/main/java/FuelPass/Dev/FuelPass/Controller/UserVerificationController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/otp")
+@RequestMapping("/api/resource/otp")
 @AllArgsConstructor
 public class UserVerificationController {
 
